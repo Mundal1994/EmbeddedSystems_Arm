64fed4ba62f1d7ff07bbf8e0041b35bec95d9b55
Mundal1994 <manga_flip@hotmail.com>
2022-11-23 13:02:46 +0200

    added buttons to stop and play wave length and switch between them. triangle wave still needs fixing

--- PWM_analogIO/main.cpp
+++ PWM_analogIO/main.cpp
@@ -1,4 +1,4 @@
-z#include "mbed.h"
+#include "mbed.h"
 
 #define SPEAKER p21
 #define AIN1 p15
@@ -9,12 +9,20 @@ PwmOut speaker(SPEAKER);
 
 // Define analog inputs
 AnalogIn ain1(AIN1);
-AnalogIN ain2(AIN2);
+AnalogIn ain2(AIN2);
+
+// Define interrupt inputs
+InterruptIn saw(p5);
+InterruptIn triangle(p6);
+InterruptIn start(p7);
+InterruptIn stop(p8);
 
 //Define variables
 float val1;
 float val2;
 float i;
+volatile unsigned int is_saw;
+volatile unsigned int is_start;
 
 /*  saw tooth wave calculation   */
 void    saw_tooth_wave()
@@ -31,42 +39,78 @@ void    triangle_wave()
 {
     for (i = 0; i < 1; i += 0.05)
     {
-        speaker.period(0.003125-(0.002*val1));
+        //speaker.period(0.003125-(0.002*val1));
         speaker = i * 0.05 * val2;
     }
     for (i = 1; i > 0; i -= 0.025)
     {
-        speaker.period(0.003125-(0.002*val1));
+        //speaker.period(0.003125-(0.002*val1));
         speaker = i * 0.05 * val2;
     }
 }
 
+void    saw_handler()
+{
+    if (!is_saw)
+    {
+        printf("switching to saw_tooth wave\n");
+        is_saw = 1;   
+    }
+}
+
+void    triangle_handler()
+{
+    if (is_saw)
+    {
+        printf("switching to triangle wave\n");
+        is_saw = 0;
+    }
+}
+
+void    start_handler()
+{
+    if (!is_start)
+    {
+        printf("play\n");
+        is_start = 1;   
+    }
+}
+
+void    stop_handler()
+{
+    if (is_start)
+    {
+        printf("paused\n");
+        is_start = 0;   
+    }
+}
+
 int main(){
+    printf("playing saw_tooth wave\n");
+    is_saw = 1;
+    is_start = 1;
+
+    // Interrupt handlers
+    saw.rise(&saw_handler);
+    triangle.rise(&triangle_handler);
+    start.rise(&start_handler);
+    stop.rise(&stop_handler);
+
     while(1){
         val1 = ain1.read();
         val2 = ain2.read();
 
-        // Create a saw-tooth sound wave
-        // the period and volume are adjustable using the potentiometers
-        saw_tooth_wave();
-
-        // create a triangle sound wave
-        // triangle_wave();
+        if (is_saw && is_start)
+        {
+            // Create a saw-tooth sound wave
+            // the period and volume are adjustable using the potentiometers
+            saw_tooth_wave();
+        }
+        else if (!is_saw && is_start)
+        {
+            // create a triangle sound wave
+            triangle_wave();
+        }
         wait_ms(100);
     }
 }
-
-/*
-to create a saw-tooth wave we could do
-for(i = 0; i < 1; i += 0.05)
-    out = i;
-    
-to create a traingle wave use two for loops:
-
-for (i = 0; i < 1; i += 0.05)
-    out = i;
-for (i = 1; i > 0; i -= 0.025)
-    out = i;
-
-
-*/
